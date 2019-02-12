8ef1ab47141609d7eb5ce179cb30a0afcb043118
sunnycase <sunnycase@live.cn>
2019-02-12 19:50:34 +0800

    Initial commit

---
+++ src/MiniDemo.Managed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniDemo.Managed
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.SayHello("Hello World from Chino OS!");
+        }
+    }
+}
---
+++ src/System.Private.CoreLib/System/Console.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace System
+{
+    public static class Console
+    {
+        public static unsafe void SayHello(string text)
+        {
+            fixed (char* p = text)
+            {
+                Runtime.RuntimeImports.SayHello(p);
+            }
+        }
+    }
+}
